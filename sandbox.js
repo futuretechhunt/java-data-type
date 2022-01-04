01870b18dcb4822bca2ad246e60b149bb3ef1d3a
Misbahur Rahman <misbahurbd@Misbahurs-MacBook-Pro.local>
2022-01-04 17:34:59 +0600

    hello

---
+++ sandbox.js
@@ -0,0 +1,23 @@
+
+
+
+let fname = "Misbahur";
+let lname = "Rahman";
+
+let fullName = fname + ' ' + lname;
+
+
+let email = "misbahurbd@gmail.com";
+
+let result = email.replace('gmail.com','yahoo.com');
+
+let mailname = email.indexOf("@");
+let ext = email.indexOf('.com');
+
+let username = email.slice(0, mailname);
+
+let data = email.slice(mailname + 1, email.length);
+console.log("username :" + username);
+console.log(" domain: " + data);
+
+console.log(email)
